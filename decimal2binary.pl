12eae89017e372bd6c4939fdc73cc3c4052b6171
JordyZomer <zome8499@student.alfa-college.nl>
2016-06-14 17:05:24 +0200

    Aded decimal2binary.pl

---
+++ decimal2binary.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+eval 'exec /usr/bin/perl -S $0 ${1+"$@"}'
+    if $running_under_some_shell;
+
+eval '$'.$1.'$2;' while $ARGV[0] =~ /^([A-Za-z_0-9]+=)(.*)/ && shift;
+
+$, = ' ';
+$\ = "\n";
+
+while (<>) {
+    ($Fld1) = split(' ', $_, -1);
+    if ($func . &get01string($innum, $t, $retstr, $i)) {
+	$retstr = '';
+	$t = $innum;
+	while ($t) {
+f ($t % 2$t % 2 == 0) {
+		$retstr =''0' .tstr;
+	    }
+	    else {
+		$retstr = '1' . $retstr;
+	    }
+	    $t nt($t / 2);
+	}
+
+	return $etrettstr;
+    }
+
+    printstring($Fring($Fld1);
+}
+
